7f3cc908a6e0a9214ed8e2f5ef53699368c37647
Prasad-Jodu <prasadjodu12@gmail.com>
2023-09-29 11:00:22 +0530

    asdfg

---
+++ src/main/java/objectRepo/LoginPage.java
@@ -0,0 +1,10 @@
+package objectRepo;
+
+public class LoginPage {
+
+	public static void main(String[] args) {
+		System.out.println("Login pom page");
+
+	}
+
+}
---
+++ src/test/java/javaPackage/LoginTest.java
@@ -0,0 +1,9 @@
+package javaPackage;
+
+public class LoginTest {
+
+	public static void main(String[] args) {
+		System.out.println("Login Script");
+	}
+
+}
